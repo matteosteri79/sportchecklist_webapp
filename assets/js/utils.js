80aea5c2490ae96e2d53f386ccef8236a3b8731e
Matteo Steri <matteo.steri@gmail.com>
2026-08-13 07:38:55 +0200

    Fix variabili utils

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,5 @@
-export const APP_VERSION = "1.7.1"
+
+export const APP_NAME = "Sport Checklist";
+export const APP_VERSION = "1.7.2"
 export const SUPPORT_EMAIL = "matteosteri+sportchecklist@gmail.com";
-export const PLAY_STORE_URL = "";
+export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.matteosteri.sportchecklist";
