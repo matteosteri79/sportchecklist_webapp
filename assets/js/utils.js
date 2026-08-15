1979de41b76ccce6b5a80922752fea7c9c654f94
Matteo Steri <matteo.steri@gmail.com>
2026-08-15 17:47:13 +0200

    v1.7.3: Modifica loghi

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,5 @@
 
 export const APP_NAME = "Sport Checklist";
-export const APP_VERSION = "1.7.2"
+export const APP_VERSION = "1.7.3"
 export const SUPPORT_EMAIL = "matteosteri+sportchecklist@gmail.com";
 export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.matteosteri.sportchecklist";
